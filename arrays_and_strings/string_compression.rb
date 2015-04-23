2bee0250bb9782b48ce8c636dfdf43cd38a75bb0
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-22 22:05:40 -0400

    Update string_compression.rb

--- arrays_and_strings/string_compression.rb
+++ arrays_and_strings/string_compression.rb
@@ -1,4 +1,7 @@
-# Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabbcccccaaa would become a2b1c5a3. If the "compressed" string would not beome smaller than the original string your method should return the original string. You can assume the string has only upper and lower case letters.
+# Implement a method to perform basic string compression using the counts of repeated characters. 
+#For example, the string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not beome smaller 
+#than the original string 
+#your method should return the original string. You can assume the string has only upper and lower case letters.
 
 
 
